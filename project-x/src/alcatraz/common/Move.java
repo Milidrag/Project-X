f5956cae698e160e8e17be4b48834224809e6c14
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-11-15 18:09:06 +0100

    +move parameters

--- project-x/src/alcatraz/common/Move.java
+++ project-x/src/alcatraz/common/Move.java
@@ -1,4 +1,11 @@
 package alcatraz.common;
 
+import java.util.UUID;
+
 public class Move {
+    private UUID moveId = UUID.randomUUID();
+
+    public UUID getMoveId() {
+        return moveId;
+    }
 }
