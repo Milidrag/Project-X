8ac830b9f4e672f9aaccb44fbb7a0480afc53fde
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-11-15 15:40:53 +0100

    +added username & user id

--- project-x/src/alcatraz/common/User.java
+++ project-x/src/alcatraz/common/User.java
@@ -1,4 +1,26 @@
 package alcatraz.common;
 
+import java.util.UUID;
+
 public class User {
+    private UUID userId ;
+
+    private String username;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
 }
